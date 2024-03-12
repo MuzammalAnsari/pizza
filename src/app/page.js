1476e26e46284b989a8e20ca7b3fe3ca94160100
MuzammalAnsari <muzammalbash@gmail.com>
2024-03-12 16:26:43 +0500

    everything in home page is ready

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Header from "@/components/layout/Header";
 import Hero from "@/components/layout/Hero";
 import HomeMenu from "@/components/layout/HomeMenu";
+import SectionHeaders from "@/components/layout/SectionHeaders";
 
 export default function Home() {
   return (
@@ -8,6 +9,47 @@ export default function Home() {
       <Header />
       <Hero />
       <HomeMenu />
+
+      <section className="text-center my-16">
+        <SectionHeaders
+          subHeader={'Our Story'}
+          mainHeader={'About Us'}
+        />
+
+        <div className='text-gray-500 max-w-md mx-auto mt-4 flex flex-col gap-4'>
+          <p>
+            Out believe has request not how comfort evident.
+            Up delight cousins we feeling minutes. Genius has looked end piqued spring.
+            Down has rose feel find man. Learning day desirous informed expenses material returned
+            six the. She enabled invited exposed him another. Reasonably conviction solicitude
+            me mr at discretion reasonable. Age out full gate bed day lose.
+          </p>
+          <p>
+            Out believe has request not how comfort evident.
+            Up delight cousins we feeling minutes. Genius has looked end piqued spring.
+            Down has rose feel find man.
+          </p>
+          <p>
+            Out believe has request not how comfort evident.
+            Up delight cousins we feeling minutes.
+          </p>
+        </div>
+      </section>
+
+      <section className="text-center my-8">
+        <SectionHeaders
+          subHeader={'Don\'t Hesitate'}
+          mainHeader={'Contact Us'}
+        />
+        <div className="mt-8">
+          <a href='123456789' className='text-4xl underline text-gray-500'>
+            123456789
+          </a>
+        </div>
+      </section>
+      <footer className="border-t p-8 text-center text-gray-500 mt-16">
+        &copy; 2024  Company, Inc. All rights reserved.
+      </footer>
     </>
   );
 }

b91b8fda1926d542de0f23cc29fb501d4a91593b
MuzammalAnsari <muzammalbash@gmail.com>
2024-03-12 15:56:47 +0500

    created the home page completely

--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -5,7 +5,7 @@ import MenuItem from "../menu/MenuItems";
 export default function HomeMenu() {
     return (
         <section className="">
-            <div className="absolute h-full left-0 right-0 w-full justify-start">
+            <div className="absolute left-0 right-0 w-full justify-start">
                 <div className="absolute left-0 -top-[70px] -z-10 text-left">
                     <Image src={'/sallad1.png'} width={109} height={189} />
                 </div>
@@ -13,11 +13,16 @@ export default function HomeMenu() {
                     <Image src={'/sallad2.png'} width={107} height={195} />
                 </div>
             </div>
-            <div className="text-center">
+            <div className="text-center mb-4">
                 <h3 className="uppercase text-gray-500 font-semibold leading-4">Check Out</h3>
                 <h2 className="text-primary font-bold text-4xl italic">Menu</h2>
             </div>
             <div className="grid grid-cols-3 gap-4">
+                <MenuItem />
+                <MenuItem />
+                <MenuItem />
+                <MenuItem />
+                <MenuItem />
                 <MenuItem />
             </div>
         </section>
--- src/components/menu/MenuItems.js
+++ src/components/menu/MenuItems.js
@@ -1,8 +1,10 @@
 export default function MenuItem() {
     return (
-        <div className="bg-gray-200 p-4 rounded-lg text-center">
-            <img src="/pizza.png" alt="pizza" />
-            <h4 className='font-semibold my-3'>Pepporoni Pizza</h4>
+        <div className="bg-gray-200 p-4 rounded-lg text-center hover:bg-white transition-all hover:shadow-md duration-300 hover:shadow-black/25">
+            <div className='text-center'>
+                <img src="/pizza.png" alt="pizza" className="max-h-auto max-h-24 block mx-auto" />
+            </div>
+            <h4 className='font-semibold my-3'>Pepperoni Pizza</h4>
             <p className='text-gray-500 text-sm'>helosdkjbckj FIUHIEWF EFFE FEFIUEFOEF QEFGE   QF     </p>
             <button className="bg-primary mt-4 rounded-full text-white px-8 py-2">Add to cart</button>
         </div>

3d2eb0cde158a529d74a0f9ec659e7270e165575
MuzammalAnsari <muzammalbash@gmail.com>
2024-03-24 16:49:39 +0500

    handle the loadings of user e.g categories

--- src/app/categories/page.js
+++ src/app/categories/page.js
@@ -1,23 +1,17 @@
 'use client'
-import { useEffect, useState } from "react";
 import UserTabs from "../../components/layout/userTabs";
-
+import { useProfile } from "../../components/layout/useProfile";
 export default function CategoriesPage() {
-    const [isAdmin, setIsAdmin] = useState(false);
-    useEffect(() => {
-        fetch('/api/profile').then(response => {
-            response.json().then(data => {
-                // console.log(data);
-                setIsAdmin(data.admin)
-            })
-        })
-    }, []);
 
-    if (!isAdmin) {
-        return 'not a admin '
+    const { loading: profileLoading, data: profileData } = useProfile();
+    if (profileLoading) {
+        return "Loading user info...";
     }
 
-
+    if (!profileData.admin) {
+        // redirect('/');
+        return <div className="text-center text-2xl text-red-500">Unauthorized</div>;
+    }
     return (
         <section className="mt-8 max-w-lg mx-auto">
             <UserTabs isAdmin={true} />
